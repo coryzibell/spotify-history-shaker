5ecd3679ca4f664b225dbc6fcc0f39a93b5a19e3
Cory Zibell <cory@zibell.cloud>
2023-03-19 23:38:55 -0400

    grooving

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { Command } from 'commander'
+// noinspection XmlDeprecatedElement,HtmlDeprecatedTag
+
+import { Command, Option } from 'commander'
 import inquirer from 'inquirer'
+import chalk from 'chalk'
+
+import packageJson from '../package.json' assert { type: 'json' }
+const version: string = packageJson.version
 
 const app = new Command()
 
@@ -8,7 +14,7 @@ const receiver = (): void => {
     { type: 'input', name: 'name', message: 'Your Name' }
   ])
     .then(answers => {
-      const name: string = answers.name
+      const name: string = chalk.blue(answers.name)
       console.log(`Hello ${name}`)
     })
     .catch(error => {
@@ -16,8 +22,26 @@ const receiver = (): void => {
     })
 }
 
-app
-  .version('1.0.0')
-  .action(receiver)
+app.addOption(
+  new Option('-d, --data <dir>', 'directory for the data exported from Spotify')
+    .default('.')
+    .env('SPOTIFY_DATA_DIRECTORY')
+)
+
+app.addOption(
+  new Option('-c, --clientid <client-id>', 'the Client ID for Spotify\'s API')
+    .env('SPOTIFY_CLIENT_ID')
+)
+
+app.addOption(
+  new Option('-s, --clientsecret <client-secret>', 'the Client Secret for Spotify\'s API')
+    .env('SPOTIFY_CLIENT_SECRET')
+)
+
+// Run the default receiver
+app.action(receiver)
+
+// Pull the version from package.json
+app.version(version)
 
 app.parse(process.argv)
